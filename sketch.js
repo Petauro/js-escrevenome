c715933751b18bbbbe7fdfad1633ba0cf3de3d23
Furãozinho <00001115691405sp@al.educacao.sp.gov.br>
2024-05-17 20:25:18 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+    //cria uma tela de 400px de largura por 400px de altura
+    createCanvas(400, 400);
+    background("white"); 
+  }
+  
+  function draw() {
+  
+    
+    stroke("blue");
+    fill("red");
+   
+    
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 35);
+    }
+  }
+  
+   
